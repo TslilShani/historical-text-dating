e0d93394f8e7256bd6b993c665c25fb4dc3d522b
Yonatan Lifshitz <yonatan@Gadis-MacBook-Pro-2.local>
2025-09-23 12:37:39 +0300

    finished anti_encoder

--- src/models/anti_encoder.py
+++ src/models/anti_encoder.py
@@ -14,7 +14,6 @@ class AntiEncoder(nn.Module):
         cfg: DictConfig,
         encoder: nn.Module,
     ):
-        """ """
         super().__init__()
 
         self.encoder = encoder
@@ -38,18 +37,22 @@ class AntiEncoder(nn.Module):
 
     def compute_loss(self, logits: torch.Tensor, labels: torch.Tensor) -> torch.Tensor:
         """
-        Compute the loss between predictions and ground truth labels.
+        Compute the loss between logits and ground truth labels.
+        Than swaps it!
 
         Args:
-            logits: Model predictions (batch_size,)
+            logits: Model logits (batch_size,)
             labels: Ground truth dates (batch_size,)
 
         Returns:
             Computed loss tensor
         """
         # Use Mean Squared Error loss for regression
-        loss_fn = nn.MSELoss()
-        return loss_fn(logits, labels.float())
+        loss_fn = nn.CrossEntropyLoss()
+        # =================================================
+        # The loss is negated to make it - "anti learning"
+        # =================================================
+        return -loss_fn(logits, labels.float())
 
     def get_model_file_name(self):
         """
