fa89931edbc46dd30393acd61c679934c661f764
tslilshani <tslilshani@mail.tau.ac.il>
2025-09-24 19:18:46 -0600

    Add slurm for grid for testing if hydra launcher works

--- src/constants.py
+++ src/constants.py
@@ -7,10 +7,10 @@ class DatasetSplitName(StrEnum):
     VALIDATION = "eval"
     TEST = "test"
 
+
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 CONFIG_DIR = os.path.join(ROOT_DIR, "configs")
 DEFAULT_CONFIG_NAME = "defaults"
 LOG_CONFIG_PATH = os.path.join(ROOT_DIR, "logging.yaml")
 RESULTS_FILE_NAME = "results.json"
 BEST_RESULTS_DIR = os.path.join(ROOT_DIR, "results", "gs_best_hyperparams")
-
